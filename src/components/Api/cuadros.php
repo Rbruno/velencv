0eb1dae0f3468a087f6ffe02b233cbe6b30f3654
Rbruno <rodrigobruno74@gmail.com>
2019-11-23 11:59:18 -0300

    make it better

--- src/components/Api/cuadros.php
+++ src/components/Api/cuadros.php
@@ -1,8 +1,8 @@
 <?php
    
-   $contenido = file_get_contents("cuadros.json");
+   /*$contenido = file_get_contents("cuadros.json");
     $con = utf8_encode($contenido);
     $datos = json_decode($con, true);
-    return($datos);
-     
+    return($datos);*/
+     echo 'hola perras';
 ?>
